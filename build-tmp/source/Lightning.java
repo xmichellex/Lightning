4f01d98470a79cf44d183dcd9a4902bca5543de7
xmichellex <michellexi123@gmail.com>
2015-09-02 10:46:51 -0700

    Added some lightning

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
 int startX = (int)(Math.random()*400); 
 int startY = 0;
 int endX = 200; 
@@ -5,14 +21,14 @@ int endY = 0;
 	
 
 
-void setup()
+public void setup()
 {
   size(400,400);
   strokeWeight(1);
   background(0, 0, 0); 
   frameRate(10);
 }
-void draw()
+public void draw()
 {
 
 	int colorX = (int)(Math.random() * 256);
@@ -51,7 +67,7 @@ clouds();
 }
 }
 
-void clouds() {
+public void clouds() {
 noStroke(); 
 fill(153); 
 ellipse(50, 20, 150, 150);
@@ -73,10 +89,19 @@ ellipse(400, 20, 150, 150);
 
 
 
-void mousePressed() {
+public void mousePressed() {
 
 background(256, 256, 256); 
 
 
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
